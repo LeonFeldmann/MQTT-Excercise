5082c0893b47eef08c25fc4640bd0add92b33f2b
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2020-11-02 17:27:39 +0100

    change mosquito to hiveMQ

--- mqttPublisher.py
+++ mqttPublisher.py
@@ -41,7 +41,7 @@ mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
 # Uncomment to enable debug messages
 # mqttc.on_log = on_log
-mqttc.connect("test.mosquitto.org", 1883, 60)
+mqttc.connect("broker.hivemq.com", 1883, 60)
 
 mqttc.loop_start()
 
--- mqttSubscriber.py
+++ mqttSubscriber.py
@@ -25,6 +25,6 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("test.mosquitto.org", 1883, 60)
+client.connect("broker.hivemq.com", 1883, 60)
 
 client.loop_forever()
